26c3246f4712be13b723f2a8b522cbc7606346dd
Chhun_Natharith <johnathanrith@gmail.com>
2025-09-17 16:42:35 +0700

    PG

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const PORT = process.env.PORT || 3008;
-
-const server = http.createServer((req, res) => {
- if (req.url === "/") {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, Node.js is working!");
- } else {
-  res.writeHead(404, { "Content-Type": "text/plain" });
-  res.end("Not Found");
- }
+app.get("/", (req, res) => {
+ res.send("Hello, Express!");
 });
 
-server.listen(PORT, () => {
- console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(port, () => {
+ console.log(`Server at http://localhost:${port}`);
 });
